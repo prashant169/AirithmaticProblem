e26206efea85ecb3b4e56a8b72b37a49616a0dc8
prashant169 <prashantphad2018@gmail.com>
2021-08-23 09:52:47 -0700

    comput  +  + /c/Users/AVENGER/bash_completion.d/*.bash

---
+++ ComputThree.sh
@@ -0,0 +1,9 @@
+#! /bin/bash -x
+
+read	-p " Enter First Number: " a
+read  -p " Enter Second Number: " b
+read  -p " Enter Third Number: " c
+
+result= $a + $b * $c
+
+echo " The Result is $result "
